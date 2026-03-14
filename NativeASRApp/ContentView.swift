eeb9fbe73d73d8d7ec4a3d54e1ef897a84a79142
morik <kosuke.mori.5120@gmail.com>
2026-03-14 11:00:09 +0900

    Initial Commit

---
+++ NativeASRApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NativeASRApp
+//
+//  Created by Kosuke Mori on 2026/03/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NativeASRApp/NativeASRAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NativeASRAppApp.swift
+//  NativeASRApp
+//
+//  Created by Kosuke Mori on 2026/03/14.
+//
+
+import SwiftUI
+
+@main
+struct NativeASRAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
